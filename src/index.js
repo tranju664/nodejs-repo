adf5af4e0baae7a626aa7d1c577d3ff1ad7bed28
tranju664 <164826479+tranju664@users.noreply.github.com>
2025-03-01 20:22:08 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 
 // Simple route
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send('Hello, World!, Welcome to Node Js Application');
 });
 
 // Start the server
